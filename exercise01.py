96f4937eccda65f4e245cdcaef0f3eb893a73b7a
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-21 14:08:59 -0400

    Edited classes, exercise01.py

--- exercise01.py
+++ exercise01.py
@@ -1,88 +1,127 @@
-class Thread:
-    
-    def __init__(self, title, time_posted, posts):
-        self.title = title
-        self.time_posted = time_posted
-        self.posts = posts
-        self.posts = []
-
-    def display(self):
-        pass
-    
-    def add_post(self, post):
-        self.posts.append(post)
-
-
-
-class Post:
-
-    def __init__(self, content, user, time_posted):
-        self.content = content
-        self.user = user
-        self.time_posted = time_posted
-        self.files = []
-
-    def display(self):
-        pass
-
-
-
-class File_post(Post):
-
-    def __init__(self, file):
-        self.file = file
-
-
+# Définit les classes exercice01 Openclassrooms
 
 
 class File:
-
+    """Fichier."""
     def __init__(self, name, size):
+        """Initialise le nom et la taille."""
         self.name = name
         self.size = size
 
     def display(self):
-        pass
+        """Affiche le fichier."""
+        print(f"Fichier '{self.name}'.")
 
 
 
 class Image(File):
+    """Fichier image."""
     pass
 
 
 
 class User:
-
+    """Utilisateur."""
     def __init__(self, username, password):
+        """Initialise le nom d'utilisateur et le mot de passe."""
         self.username = username
         self.password = password
 
-    def sign_in(self):
-        pass
-
     def login(self):
-        pass
+        """Connecte l'utilisateur."""
+        print(f"L'utilisateur {self.username} est connecté.")
 
     def logout(self):
         pass
 
-    def post(self, thread, content):
-        pass
+    def post(self, thread, content, file=None):
+        """Poste un message dans un fil de discussion."""
+        if file:
+            post = FilePost(self, "aujourd'hui", content, file)
+        else:
+            post = Post(user=self, time_posted="aujourd'hui", content=content)
+        thread.add_post(post)
+        return post
 
-    def make_thread(title, content):
-        pass
+    def make_thread(self, title, content):
+        """Crée un nouveau fil de discussion."""
+        post = Post(self, "aujourd'hui", content)
+        return Thread(title, "aujourd'hui", post)
+    
+    def __str__(self):
+        """Représentation de l'utilisateur."""
+        return self.username
 
 
 
 class Moderator(User):
-
-    def __init__(self):
-        pass
-
+    """Utilisateur modérateur."""
     def edit(self, post, content):
-        pass
+        """Modifie un message."""
+        post.content = content
 
     def delete(self, thread, post):
-        pass
+        """Supprime un message."""
+        index = thread.posts.index(post)
+        del thread.posts[index]
 
- 
+
+
+class Post:
+    """Message."""
+    def __init__(self, user, time_posted, content):
+        """Initialise l'utilisateur, la date et le contenu."""
+        self.user = user
+        self.time_posted = time_posted
+        self.content = content
+
+    def display(self):
+        """Affiche le message."""
+        print(f"Message posté par: {self.user} le {self.time_posted}.")
+        print(self.content)
+
+
+
+class FilePost(Post):
+    """Message comportant un fichier."""
+    def __init__(self, user, time_posted, content, file):
+        """Initialise le fichier."""
+        self.user = user
+        self.time_posted = time_posted
+        self.content = content
+        self.file = file
+
+    def display(self):
+        """Affiche le contenu et le fichier."""
+        super().display()
+        print("pièce jointe:")
+        self.file.display()
+
+
+
+class Thread:
+    """Fil de discussion."""
+    def __init__(self, title, time_posted, posts):
+        """Initialise le titre, la date et les posts.
+
+        Attention ici: on commence par un seul post, celui du sujet.
+        Les réponses à ce post ne pourrons s'ajouter qu'ultérieurement.
+        En effet, on ne créé pas directement un nouveau fil avec des réponses."""
+
+        self.title = title
+        self.time_posted = time_posted
+        self.posts = [posts]
+
+    def display(self):
+        """Affiche le fil de discussion."""
+        print("----- THREAD -----")
+        print(f"titre: {self.titre}, date: {self.time_posted}")
+        print()
+        for post in self.posts:
+            post.display()
+            print()
+        print("---------------")
+    
+    def add_post(self, post):
+        """Ajoute un post."""
+        self.posts.append(post)
